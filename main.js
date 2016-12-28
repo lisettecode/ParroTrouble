20fa516e70c98672c83d36f543c35b841ea71a21
Liichy Aguilar Bueno <Liichy Aguilar Bueno>
2016-12-28 11:26:36 -0500

    ParrotImg

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-function parrotTrouble() 
+function LoroA() 
 {
     var img = document.getElementById("Loro");
-    var Checkbox = document.getElementById("Loro");
+    var Checkbox = document.getElementById("Loro_a");
     var diff = 0;    
     
     if (Checkbox.checked)
         {
-            img.innerHTML = "<img src= 'img/loro_cantor.jpg'>";
+            img.innerHTML = '<img src="img/loro_cantor.jpg">';
         }
 
     else
       {
-            img.innerHTML = "<img src= 'img/loro_durmiendo.jpg'>";
+            img.innerHTML = '<img src="img/loro_durmiendo.jpg">';
     }
  
 
@@ -22,7 +22,7 @@ function parrotTrouble()
 
 function evaluar() 
 {
-    var aTalking = document.getElementById("Loro").checked;
+    var aTalking = document.getElementById("Loro_a").checked;
     var parrotTrouble = document.getElementById("salida");
     var hora = document.getElementById("hora").value;
     
